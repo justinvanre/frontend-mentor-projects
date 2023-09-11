ae2a00f867e1c827291b60e067ef86f0cfe36e32
Justin Vanre <justin.vr@startmail.com>
2023-09-11 15:53:07 +0200

    Refactored form validationintro component with sign up form

--- intro-component-with-signup-form/app.js
+++ intro-component-with-signup-form/app.js
@@ -7,28 +7,19 @@ const validateForm = () => {
     // criteria of how an email should look like
     const emailCriteria = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-    /* 
-        Logic checking if the fields are empty 
-    */
     requiredElements.forEach(element => {
-        if(element.value.trim() === "") {
+        if(element.value.trim() === "" || (element.type === 'email' && !emailCriteria.test(element.value))) { // Logic checking if the fields are empty
+
             // prevent form from submitting 
             isValid = false; 
 
             // Enable styling error state
             element.classList.add('cta__sign-up__form__input--error');
-            let siblingEl = element.nextElementSibling; 
-            siblingEl.innerHTML = `${element.name} cannot be empty.`;
-            siblingEl.style.display = 'block';
-
-        } else if (element.type = 'email' && !emailCriteria.test(element)) {
-            // logic to check if email is well formulated
-            isValid = false;
-            element.classList.add('cta__sign-up__form__input--error');
-
-            let siblingEl = element.nextElementSibling; 
-            siblingEl.innerHTML = `Looks like this is not an email`;
-            siblingEl.style.display = 'block';
+                        
+            let nextEl = element.nextElementSibling; 
+            nextEl.style.display = 'block'; 
+            nextEl.innerHTML = element.type === 'email' ? `Looks like this is not an email.`:
+                                                          `${element.name} cannot be empty.`;
 
         } else {
             // disable styling error state
@@ -39,3 +30,4 @@ const validateForm = () => {
     // return value to submit or prevent submitting from.
     return isValid;
 }
+
