1cb1a0d344b0028677ab887412b08503e733236a
Justin Vanre <justin.vr@startmail.com>
2023-09-11 14:06:21 +0200

    Logic email-check intro component with login form

--- intro-component-with-signup-form/app.js
+++ intro-component-with-signup-form/app.js
@@ -4,6 +4,9 @@ const validateForm = () => {
 
     const requiredElements = form.querySelectorAll('[required]');
 
+    // criteria of how an email should look like
+    const emailCriteria = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
     /* 
         Logic checking if the fields are empty 
     */
@@ -16,7 +19,17 @@ const validateForm = () => {
             element.classList.add('cta__sign-up__form__input--error');
             let siblingEl = element.nextElementSibling; 
             siblingEl.innerHTML = `${element.name} cannot be empty.`;
-            siblingEl.style.display = 'block';             
+            siblingEl.style.display = 'block';
+
+        } else if (element.type = 'email' && !emailCriteria.test(element)) {
+            // logic to check if email is well formulated
+            isValid = false;
+            element.classList.add('cta__sign-up__form__input--error');
+
+            let siblingEl = element.nextElementSibling; 
+            siblingEl.innerHTML = `Looks like this is not an email`;
+            siblingEl.style.display = 'block';
+
         } else {
             // disable styling error state
             element.classList.remove('cta__sign-up__form__input--error');
