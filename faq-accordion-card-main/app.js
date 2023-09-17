b4d540c2789a12dbb4f205d6b7a0a765e5510762
Justin Vanre <justin.vr@startmail.com>
2023-09-17 20:33:04 +0200

    improve code toggle mechanism faq card

--- faq-accordion-card-main/app.js
+++ faq-accordion-card-main/app.js
@@ -1,31 +1,40 @@
 const questions = document.querySelectorAll('.faq-card__list__question'); 
 
+const showAnswer = (question, answer) => {
+    answer.style.display = 'block';
+    // rotation arrow
+    question.nextElementSibling.style.transform = 'rotate(180deg)';
+    question.style.fontWeight = '700'; 
+}
+
+
+const hideAnswer = (question, answer) => {
+    answer.style.display = 'none';
+    question.nextElementSibling.style.transform = 'unset';
+    question.style.fontWeight = '400'; 
+}
+
+
 questions.forEach(question => {
 
     question.addEventListener('click', () => {
         
         // toggle answer of question
-        const arrow =  question.firstElementChild;
-        const answer = question.nextElementSibling;
+        let answer = question.nextElementSibling;
 
         // style question clicked
         if (answer.style.display === 'block') {
-            answer.style.display = 'none';
-            arrow.style.transform = 'unset';
-            question.style.fontWeight = '400'; 
+            hideAnswer(question, answer);
         } else {
-            answer.style.display = 'block';
-            arrow.style.transform = 'rotate(180deg)';
-            question.style.fontWeight = '700'; 
+            showAnswer(question, answer);
         }
         
         // close questions that are open
         questions.forEach(q => {
             if (q !== question) {
                 // undo styling from toggle state
-                q.nextElementSibling.style.display = 'none';
-                q.style.fontWeight = '400';
-                q.firstElementChild.style.transform = 'unset';
+                answer = q.nextElementSibling;
+                hideAnswer(q, answer);
             }
         });
 
