1883a2bb91bd83de81d59fb0322fe4dddcf58b24
Justin Vanre <justin.vr@startmail.com>
2023-09-17 19:47:44 +0200

    fix toggle mechanism faq card

--- faq-accordion-card-main/app.js
+++ faq-accordion-card-main/app.js
@@ -5,13 +5,19 @@ questions.forEach(question => {
     question.addEventListener('click', () => {
         
         // toggle answer of question
+        const arrow =  question.firstElementChild;
         const answer = question.nextElementSibling;
-        answer.style.display = 'block';
 
-        // style question   
-        question.style.fontWeight = '700';
-        question.firstElementChild.style.transform = 'rotate(180deg)';
-
+        // style question clicked
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+            arrow.style.transform = 'unset';
+            question.style.fontWeight = '400'; 
+        } else {
+            answer.style.display = 'block';
+            arrow.style.transform = 'rotate(180deg)';
+            question.style.fontWeight = '700'; 
+        }
         
         // close questions that are open
         questions.forEach(q => {
@@ -19,7 +25,7 @@ questions.forEach(question => {
                 // undo styling from toggle state
                 q.nextElementSibling.style.display = 'none';
                 q.style.fontWeight = '400';
-                q.firstElementChild.style.transform = 'unset'
+                q.firstElementChild.style.transform = 'unset';
             }
         });
 
