3a361f537a7c0aaca35cd0b9822ede5f58db7fd5
Justin Vanre <justin.vr@startmail.com>
2023-10-23 11:10:14 +0200

    Refactor toggling mechanism mobile menu

--- blogr-landing-page-main/app.js
+++ blogr-landing-page-main/app.js
@@ -1,7 +1,12 @@
 
 const menuIcon = document.querySelector('.menu__icon img');
+const nav = document.querySelector('.nav .wrapper');
+
+console.log(nav); 
 
 menuIcon.addEventListener('click', (e) => {
+
+    nav.classList.toggle('clicked');
 
     if (menuIcon.src.endsWith("hamburger.svg")) {
         menuIcon.src = "./assets/images/icon-close.svg";
