9ab0e8debeb552d092ee2e4986f76354114fa7bd
Justin Vanre <justin.vr@startmail.com>
2023-10-31 11:27:28 +0100

    Fix toggle mechanism menu component

--- blogr-landing-page-main/app.js
+++ blogr-landing-page-main/app.js
@@ -1,12 +1,14 @@
 
 const menuIcon = document.querySelector('.menu__icon img');
-const nav = document.querySelector('.nav .wrapper');
+const nav = document.querySelector('.nav');
+const body = document.querySelector('body');
 
 console.log(nav); 
 
 menuIcon.addEventListener('click', (e) => {
 
     nav.classList.toggle('clicked');
+    body.classList.toggle('modal-open');
 
     if (menuIcon.src.endsWith("hamburger.svg")) {
         menuIcon.src = "./assets/images/icon-close.svg";
